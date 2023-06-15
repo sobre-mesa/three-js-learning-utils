59a47c6b69c99adc60ce3b3f26fb604b65b61b04
Nicolas Videla <gargantuan_ape@nvidela-mb.ht.home>
2023-06-15 16:24:27 -0300

    Added Controls

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { makeCube, ratio, makeCamera, makeRenderer, addPerformanceStats } from './clientUtils';
-
+import { GUI } from 'dat.gui';
 
 const scene = new THREE.Scene();
 const camera = makeCamera();
@@ -11,6 +11,22 @@ scene.add(cube);
 const stats = addPerformanceStats();
 new OrbitControls(camera, renderer.domElement);
 
+export function addGUI(){
+    const gui = new GUI();
+
+    const cubeFolder = gui.addFolder("Cube Position");
+    cubeFolder.add(cube.rotation, 'x', 0, Math.PI * 2);
+    cubeFolder.add(cube.rotation, 'y', 0, Math.PI * 2);
+    cubeFolder.add(cube.rotation, 'z', 0, Math.PI * 2);
+
+    const cameraFolder = gui.addFolder("Camera Position");
+    cameraFolder.add(camera.position, 'x', 0, 10);
+    cameraFolder.add(camera.position, 'y', 0, 10);
+    cameraFolder.add(camera.position, 'z', 0, 10);
+}
+
+
+addGUI();
 
 function render() {
     renderer.render(scene, camera);
--- src/client/clientUtils.ts
+++ src/client/clientUtils.ts
@@ -32,3 +32,4 @@ export function addPerformanceStats(){
     document.body.appendChild(stats.dom);
     return stats;
 }
+
