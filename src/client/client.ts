c767b63ea38ef2d40eb3e3600cc3b327ff32790a
Nicolas Videla <gargantuan_ape@nvidela-mb.ht.home>
2023-06-15 16:16:21 -0300

    Added Stats Panel

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,18 +1,16 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import {makeCube, ratio} from './clientUtils';
+import { makeCube, ratio, makeCamera, makeRenderer, addPerformanceStats } from './clientUtils';
+
 
 const scene = new THREE.Scene();
-
-const camera = new THREE.PerspectiveCamera(75, ratio(), 0.1, 1000);
-camera.position.z = 2;
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
+const camera = makeCamera();
+const renderer = makeRenderer()
 const cube = makeCube();
 scene.add(cube);
+const stats = addPerformanceStats();
+new OrbitControls(camera, renderer.domElement);
+
 
 function render() {
     renderer.render(scene, camera);
@@ -26,14 +24,13 @@ function onWindowResize() {
 }
 
 window.addEventListener('resize', onWindowResize, false);
-new OrbitControls(camera, renderer.domElement);
+
 
 function animate() {
     requestAnimationFrame(animate);
-
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
-
+    stats.update();
     render();
 }
 
--- src/client/clientUtils.ts
+++ src/client/clientUtils.ts
@@ -1,4 +1,5 @@
 import * as THREE from 'three'
+import Stats from 'three/examples/jsm/libs/stats.module';
 
 export function makeCube() {
     const geometry = new THREE.BoxGeometry()
@@ -12,3 +13,22 @@ export function makeCube() {
 export function ratio(){
     return window.innerWidth / window.innerHeight;
 } 
+
+export function  makeCamera(){
+    const camera = new THREE.PerspectiveCamera(75, ratio(), 0.1, 1000);
+    camera.position.z = 2;
+    return camera;
+}
+
+export function makeRenderer(){
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+    return renderer;
+}
+
+export function addPerformanceStats(){
+    const stats = new Stats();
+    document.body.appendChild(stats.dom);
+    return stats;
+}
